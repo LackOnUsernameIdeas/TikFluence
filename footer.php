da6e5ef4c453ae92a249db1b2da1493e425bd04c
LackOnUsernameIdeas <121722916+LackOnUsernameIdeas@users.noreply.github.com>
2023-02-16 16:11:28 +0200

    fixed footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="bottom-footer text-center py-md-4 py-3">
         <h3>
-            НОИТ 2023 | Проект №423 | Уеб сайт | Калоян Костадинов и Николай Георгиев  |
+            НОИТ 2023 | Проект №423 | Софтуерни приложения | Калоян Костадинов и Николай Георгиев  |
             <a href="https://pgi-pernik.bg-schools.com" target="_blank">ПГИ - гр. Перник</a>
         </h3>
     </div>
