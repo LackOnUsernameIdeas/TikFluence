b1460bb8e4a1a44b7332a417de7dbe86a23b0706
LackOnUsernameIdeas <121722916+LackOnUsernameIdeas@users.noreply.github.com>
2023-02-19 06:21:09 +0200

    Resultatut za oblastniq

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <footer>
     <div class="bottom-footer text-center py-md-4 py-3">
-        <h3>
+        <h4>
             НОИТ 2023 | Проект №423 | Софтуерни приложения | Калоян Костадинов и Николай Георгиев  |
             <a href="https://pgi-pernik.bg-schools.com" target="_blank">ПГИ - гр. Перник</a>
-        </h3>
+        </h4>
     </div>
 </footer>
